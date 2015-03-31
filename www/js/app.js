68653ab948a75fa4068b63f392e85a24325edf60
stbyrne <strtbyrne@gmail.com>
2015-03-31 17:51:00 +0100

    commit_home_45

--- www/js/app.js
+++ www/js/app.js
@@ -12,12 +12,14 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives']
     // for form inputs)
     navigator.splashscreen.hide();
       
+    
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
+      StatusBar.overlaysWebView();
     }
   });
 })
