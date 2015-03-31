0db55547a03a577496fb7f8458d20e9a253b0c38
stbyrne <strtbyrne@gmail.com>
2015-03-31 18:10:28 +0100

    commit_home_47

--- www/js/app.js
+++ www/js/app.js
@@ -16,10 +16,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives']
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
-    }
+    
   });
 })
 
