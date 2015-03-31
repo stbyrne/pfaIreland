c5afed9c35564d1ad0ff92b07a68411d0a428309
stbyrne <strtbyrne@gmail.com>
2015-03-31 16:19:08 +0100

    commit_home_40

--- www/js/app.js
+++ www/js/app.js
@@ -10,6 +10,8 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives']
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
+      navigator.splashscreen.hide();
+      
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
