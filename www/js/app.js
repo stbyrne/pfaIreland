d04086966773c95c971893688f14f8f715bf8667
stbyrne <strtbyrne@gmail.com>
2015-03-31 16:38:22 +0100

    commit_home_42

--- www/js/app.js
+++ www/js/app.js
@@ -14,7 +14,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives']
       
     if(window.StatusBar) {
       // org.apache.cordova.statusbar required
-      /*StatusBar.styleDefault();*/
+        StatusBar.styleDefault();
         StatusBar.hide();
     }
     if (window.cordova && window.cordova.plugins.Keyboard) {
